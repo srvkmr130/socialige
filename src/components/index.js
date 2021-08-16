8161305501972cc4cd4b5a8c3f80fe6d10c7b3ca
srvkmr130@ <srvkmr130@gmail.com>
2021-08-16 16:43:22 +0530

    feature to post comment

--- src/components/index.js
+++ src/components/index.js
@@ -10,6 +10,8 @@ import UserProfile from './UserProfile';
 import FriendsList from './FriendsList';
 import FriendsListItem from './FriendsListItem';
 import CreatePost from './CreatePost';
+import Comment from './Comment';
+import Post from './Post';
 
 export {
   PostsList,
@@ -24,4 +26,6 @@ export {
   FriendsListItem,
   FriendsList,
   CreatePost,
+  Comment,
+  Post,
 };
